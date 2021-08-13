c199e647861c640e6c2953cfac647799a753c9a0
cs13engineer <75882819+cs13engineer@users.noreply.github.com>
2021-08-13 13:48:46 +0530

    Caclulator Applications backend processing

---
+++ calci.go
@@ -0,0 +1,42 @@
+package calci
+
+//will add integers
+func addInt(i int, j int)int{
+	return(i+j)
+}
+
+//will add float32
+func addFloat(i float32, j float32)float32{
+	return(i+j)
+}
+
+//will subtract integers
+func subInt(i int, j int)int{
+	return(i-j)
+}
+
+//will subtract float32
+func subFloat(i float32, j float32)float32{
+	return(i-j)
+}
+
+//will multiply integers
+func subMul(i int, j int)int{
+	return(i*j)
+}
+
+//will multiply float32
+func subMul(i float32, j float32)float32{
+	return(i*j)
+}
+
+//will divide integers
+func subDiv(i int, j int)int{
+	return(i/j)
+}
+
+//will divide the Float32 type
+func subDiv(i float32, j float32)float32{
+	return(i/j)
+}
+
